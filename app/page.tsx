6830fb76f4732ada02e183338756fc68fb759cb8
NisargKaneriya-Prioxis <Nisarg@prioxis.com>
2025-12-09 10:28:29 +0530

    direct link to bot

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,8 @@
 import Link from "next/link";
+import { redirect } from "next/navigation";
+
  
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold mb-4">Welcome</h1>
-      <Link
-        href="/chat"
-        className="text-blue-500 underline text-xl"
-      >
-        Go to Chatbot
-      </Link>
-    </main>
-  );
+   redirect('/chat');
 }
  
